53363026d6460ca0799039fa64725a2f4ae91860
emischen02 <emis.chen@sjsu.edu>
2025-09-11 13:25:34 -0700

    demo

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    alert('Hello from script.js!');
+    let userName = 'John Doe';
+    const userAge = 30;
+    var isActiveUser = true;
+});
